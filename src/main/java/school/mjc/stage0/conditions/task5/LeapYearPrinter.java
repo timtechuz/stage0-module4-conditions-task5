4542e473ebfbd658a788ef1f26567c0c4f34e8e9
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-25 09:26:36 +0500

    stage0-module4-conditions-task5 first attempt

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,29 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+    	if (year%4==0) {
+    		if (year%100==0) {
+    			if (year%400==0) {
+    				// year is century and leap
+    				System.out.println("leap");
+    			} else {
+    				// the year is century and not leap
+    				System.out.println("not leap");
+    			}
+    		} else
+    			// the year is not century and leap 
+    			System.out.println("leap");
+    		
+    	} else {
+    		System.out.println("not leap");
+    	}
     }
+    
+    /*
+    public static void main(String[] args) {
+    	LeapYearPrinter lyp = new LeapYearPrinter();
+    	lyp.isLeapYear(2300);
+    }
+    */
 }
+
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,60 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+    	switch (month) {
+    		case 1:
+    			System.out.println(31);
+    			break;
+    		case 2:
+    			System.out.println(28);
+    			break;
+    		case 3:
+    			System.out.println(31);
+    			break;
+    		case 4:
+    			System.out.println(30);
+    			break;
+    		case 5:
+    			System.out.println(31);
+    			break;
+    		case 6:
+    			System.out.println(30);
+    			break;
+    		case 7:
+    			System.out.println(31);
+    			break;
+    		case 8:
+    			System.out.println(31);
+    			break;
+    		case 9:
+    			System.out.println(30);
+    			break;
+    		case 10:
+    			System.out.println(31);
+    			break;
+    		case 11:
+    			System.out.println(30);
+    			break;
+    		case 12:
+    			System.out.println(31);
+    			break;
+    		default:
+    			System.out.println("wrong number!");
+    			break;
+    	}
+    	/*if (month == 1) { System.out.println(31); }
+    	else if (month == 2) { System.out.println(28); }
+    	else if (month == 3) { System.out.println(31); }
+    	else if (month == 4) { System.out.println(30); }
+    	else if (month == 5) { System.out.println(31); }
+    	else if (month == 6) { System.out.println(30); }
+    	else if (month == 7) { System.out.println(31); }
+    	else if (month == 8) { System.out.println(31); }
+    	else if (month == 9) { System.out.println(30); }
+    	else if (month == 10) { System.out.println(31); }
+    	else if (month == 11) { System.out.println(30); }
+    	else if (month == 12) { System.out.println(31); }
+    	else { System.out.println("wrong number!"); }
+    	*/
     }
 }
