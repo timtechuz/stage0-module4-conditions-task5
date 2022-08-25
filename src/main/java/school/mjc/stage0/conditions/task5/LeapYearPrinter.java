fc41eb3da84d0f05a2e0965cf137c10b980eeb1d
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-25 15:15:29 +0500

    stage0-module4-conditions-task5 second attempt: System.out replaced by logger.info() for test.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,30 +1,27 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.util.logging.Logger;
+
 public class LeapYearPrinter {
+	Logger log = Logger.getLogger(LeapYearPrinter.class.getName());
     public void isLeapYear(int year) {
     	if (year%4==0) {
     		if (year%100==0) {
     			if (year%400==0) {
     				// year is century and leap
-    				System.out.println("leap");
+    				log.info("leap");
     			} else {
     				// the year is century and not leap
-    				System.out.println("not leap");
+    				log.info("not leap");
     			}
     		} else
     			// the year is not century and leap 
-    			System.out.println("leap");
+    			log.info("leap");
     		
     	} else {
-    		System.out.println("not leap");
+    		log.info("not leap");
     	}
     }
-    
-    /*
-    public static void main(String[] args) {
-    	LeapYearPrinter lyp = new LeapYearPrinter();
-    	lyp.isLeapYear(2300);
-    }
-    */
+  
 }
 
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -43,19 +43,5 @@ public class SwitchDaysInMonthPrinter {
     			System.out.println("wrong number!");
     			break;
     	}
-    	/*if (month == 1) { System.out.println(31); }
-    	else if (month == 2) { System.out.println(28); }
-    	else if (month == 3) { System.out.println(31); }
-    	else if (month == 4) { System.out.println(30); }
-    	else if (month == 5) { System.out.println(31); }
-    	else if (month == 6) { System.out.println(30); }
-    	else if (month == 7) { System.out.println(31); }
-    	else if (month == 8) { System.out.println(31); }
-    	else if (month == 9) { System.out.println(30); }
-    	else if (month == 10) { System.out.println(31); }
-    	else if (month == 11) { System.out.println(30); }
-    	else if (month == 12) { System.out.println(31); }
-    	else { System.out.println("wrong number!"); }
-    	*/
     }
 }
